e0ea1ed364ce46c3ae1067b859124a7ce6f3bdfe
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2024-10-18 13:08:59 +0300

    delete all text

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,8 +3,5 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Lesson Git");
-        System.out.println("Change commit");
-        System.out.println("Bla-bla");
     }
 }
